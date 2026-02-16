f7792ca4a8aa6ffffcf83add3a421b0826f92763
mfat7y2005-collab <m.fat7y2005@gmail.com>
2026-02-16 23:14:36 +0200

    Delete Desktop directory

--- Desktop/jeson/mongo/src/DB/model/user.model.js
+++
@@ -1,4 +0,0 @@
-export const users =[]
-import { db } from '../connection.db.js';
-
-export const userModel = db.createCollection(" Users")
--- Desktop/jeson/mongo/src/modules/user/user.service.js
+++
@@ -1,5 +0,0 @@
-import {users} from '../../DB/model/index.js'
-export const profile   = (id)=>{
-    const user = users.find(ele => ele.id == id)
-    return user
-}
